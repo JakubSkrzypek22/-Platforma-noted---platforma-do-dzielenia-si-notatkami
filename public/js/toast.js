d74d10e7a2bc5e5a6733cbd4eb9ddd7783062470
Jakub_Skrzypek <JakubSkrzypek22>
2026-05-17 20:58:39 +0200

    Initial commit

---
+++ public/js/toast.js
@@ -0,0 +1,7 @@
+window.onload = function (e) {
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+    var toastList = toastElList.map(function (toastEl) {
+        return new bootstrap.Toast(toastEl)
+    })
+    toastList.forEach(toast => toast.show())
+}
